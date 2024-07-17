e12b53fa6e119f4c7045d624ed426415103fb7bf
Pranav <pranavkhatade02@gmail.com>
2024-07-17 23:56:42 +0530

    Add files via upload

---
+++ cal.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+
+using name space std;
+
+class Cal
+{
+
+  public:
+   float a,b 
+   
+   float display_sum()
+   {
+    return a+b;
+
+   }
+   float display_sub()
+   {
+    return a-b;
+   }
+
+   float display_mul()
+   {
+    return a*b;
+   }
+
+   float display_div()
+   {
+    return a/b;
+   }
+
+}:
+
+int main() 
+{
+  Cal c1;
+
+  cin>>a;
+  cin>>b;
+
+  cout<<"Addition is :"<<c1.display_sum()<<endl;
+  cout<<"Subtraction is :"<<c1.display_sub()<<endl;
+  cout<<"multiplication is :"<<c1.display_mul()<<endl;
+  cout<<"Division is :"<<c1.display_div()<<endl;
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
